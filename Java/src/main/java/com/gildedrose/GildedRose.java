a93899917052b37a7371a0dc22e9f0d882382631
bart.biemans <bart.biemans@corilus.be>
2024-11-19 21:28:15 +0100

    Add early return in case of sulfuras item because it never changes

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,12 +13,13 @@ class GildedRose {
         final String sulfuras = "Sulfuras, Hand of Ragnaros";
 
         for (Item item : items) {
+            if(item.name.equals(sulfuras)){
+                continue;
+            }
             if (!item.name.equals(AgedBrie)
                 && !item.name.equals(backStagePass)) {
                 if (item.quality > 0) {
-                    if (!item.name.equals(sulfuras)) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.quality = item.quality - 1;
                 }
             } else {
                 if (item.quality < 50) {
@@ -40,17 +41,13 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals(sulfuras)) {
-                item.sellIn = item.sellIn - 1;
-            }
+            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (!item.name.equals(AgedBrie)) {
                     if (!item.name.equals(backStagePass)) {
                         if (item.quality > 0) {
-                            if (!item.name.equals(sulfuras)) {
-                                item.quality = item.quality - 1;
-                            }
+                            item.quality = item.quality - 1;
                         }
                     } else {
                         item.quality = 0;
