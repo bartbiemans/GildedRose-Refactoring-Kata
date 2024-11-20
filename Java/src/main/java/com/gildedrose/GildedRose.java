383bdb75d12f25d4e0ad79ed953ad5586355bc65
bart.biemans <bart.biemans@corilus.be>
2024-11-20 07:52:15 +0100

    Make it so other methods never alters the object and only calculates the quality field.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,51 +12,46 @@ class GildedRose {
         final String backStagePass = "Backstage passes to a TAFKAL80ETC concert";
         final String sulfuras = "Sulfuras, Hand of Ragnaros";
 
-        for (Item item : items) {
+        for (final Item item : items) {
             if (item.name.equals(sulfuras)) {
                 continue;
             }
-            switch (item.name) {
-                case AgedBrie -> updateAgedBrie(item);
-                case backStagePass -> updateBackStagePass(item);
-                default -> updateDefaultItem(item);
-            }
-        }
-    }
 
-    private void updateDefaultItem(final Item item) {
-        if(item.quality <= 0 ) {
+            item.quality = switch (item.name) {
+                case AgedBrie -> calculateQualityForAgedBrie(item);
+                case backStagePass -> calculateQualityForBackStagePass(item);
+                default -> calculateQualityForDefaultItem(item);
+            };
             item.sellIn = item.sellIn - 1;
-            return;
+        }
+    }
+
+    private int calculateQualityForDefaultItem(final Item item) {
+        if (item.quality == 0) {
+            return 0;
         }
 
-        item.quality = item.quality - 1;
-        item.sellIn = item.sellIn - 1;
-
-        if (item.sellIn < 0) {
+        if (item.sellIn <= 0) {
             if (item.quality > 0) {
-                item.quality = item.quality - 1;
+                return item.quality - 2;
             }
         }
+        return item.quality - 1;
     }
 
-    private void updateAgedBrie(final Item agedBrie) {
-        if (agedBrie.quality < 50) {
-            agedBrie.quality++;
-        }
-        agedBrie.sellIn = agedBrie.sellIn - 1;
+    private int calculateQualityForAgedBrie(final Item agedBrie) {
+        return agedBrie.quality < 50 ? agedBrie.quality + 1 : agedBrie.quality;
     }
 
-    private void updateBackStagePass(final Item backStagePass) {
+    private int calculateQualityForBackStagePass(final Item backStagePass) {
         if (backStagePass.sellIn > 10) {
-            backStagePass.quality++;
+            return backStagePass.quality + 1;
         } else if (backStagePass.sellIn > 5) {
-            backStagePass.quality += 2;
+            return backStagePass.quality + 2;
         } else if (backStagePass.sellIn > 0) {
-            backStagePass.quality += 3;
+            return backStagePass.quality + 3;
         } else {
-            backStagePass.quality = 0;
+            return backStagePass.quality = 0;
         }
-        backStagePass.sellIn--;
     }
 }
